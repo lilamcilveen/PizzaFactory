0a53d3076b9890eeb062c2c309f345ff770537a3
Iqra McIlveen <iqramcilveen@gmail.com>
2018-12-05 06:37:54 -0500

    Update HawaiianPizza.java

--- src/PizzaManagement/HawaiianPizza.java
+++ src/PizzaManagement/HawaiianPizza.java
@@ -4,8 +4,8 @@ public class HawaiianPizza implements iPizza {
     @Override
     public void MakePizza() {
         System.out.println("Adding tomato sauce and cheese");
-        System.out.println("Adding pineapple, back bacon, and ham.");
-        System.out.println("Firing the oven.");
+        System.out.println("Adding pineapple, back bacon, and ham");
+        System.out.println("Firing the oven");
         System.out.println();
         System.out.println("Hawaiian pizza is ready!");
 
