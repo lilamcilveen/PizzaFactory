9676be24a560a40c906bd9af6b152e3583748e13
Iqra McIlveen <iqramcilveen@gmail.com>
2018-12-05 06:37:28 -0500

    Update CheesePizza.java

--- src/PizzaManagement/CheesePizza.java
+++ src/PizzaManagement/CheesePizza.java
@@ -4,7 +4,7 @@ public class CheesePizza implements iPizza{
     @Override
     public void MakePizza() {
         System.out.println("Adding tomato sauce and cheese");
-        System.out.println("Firing the oven.");
+        System.out.println("Firing the oven");
         System.out.println();
         System.out.println("Cheese pizza is ready!");
 
