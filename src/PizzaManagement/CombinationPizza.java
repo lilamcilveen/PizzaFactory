71f30c9ec8154542d0d000c82d24f5a2b938400b
Iqra McIlveen <iqramcilveen@gmail.com>
2018-12-05 06:37:42 -0500

    Update CombinationPizza.java

--- src/PizzaManagement/CombinationPizza.java
+++ src/PizzaManagement/CombinationPizza.java
@@ -5,8 +5,8 @@ public class CombinationPizza implements iPizza {
     @Override
     public void MakePizza() {
         System.out.println("Adding tomato sauce and cheese");
-        System.out.println("Adding pepperoni, mushrooms, and bell pepper.");
-        System.out.println("Firing the oven.");
+        System.out.println("Adding pepperoni, mushrooms, and bell pepper");
+        System.out.println("Firing the oven");
         System.out.println();
         System.out.println("Combination pizza is ready!");
 
